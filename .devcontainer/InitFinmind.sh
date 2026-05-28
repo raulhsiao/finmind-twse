d4aec53661b5462e1bf594b3c4a756287ca0e154
Raul Hsiao <Raul_Hsiao@chicony.com>
2026-05-28 11:09:16 +0000

    append dataset  spec of finmind to claude

--- .devcontainer/InitFinmind.sh
+++ .devcontainer/InitFinmind.sh
@@ -2,8 +2,21 @@
 
 echo "[startup] Using current path: $DEVC_VENV_PATH"
 
+mkdir -p /workspace/packages
+
+pushd /workspace/packages
+git clone https://github.com/FinMind/FinMind.git
+pushd /workspace/packages/FinMind
+git branch
+cp -av /workspace/packages/FinMind/.claude/commands/* ~/.claude/commands/
+popd
+popd
+
 export UV_VENV_CLEAR=1
 uv venv finmind 
 # source finmind/bin/activate 
-uv pip install finmind pandas numpy jupyterlab requests matplotlib \
+uv pip install -r /workspace/packages/FinMind/requirements.txt \
    --python finmind
+uv pip install finmind jupyterlab requests matplotlib \
+   --python finmind
+
--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -16,13 +16,3 @@ echo "[startup] Using current path: $DEVC_VENV_PATH"
 # Each process when every time devcontainer creating
 .devcontainer/InitFinmind.sh
 .devcontainer/InitGitEnv.sh
-
-mkdir -p ~/.claude/commands
-curl -o ~/.claude/commands/finmind.md \
-  https://raw.githubusercontent.com/FinMind/FinMind/master/.claude/commands/finmind.md
-
-mkdir -p /workspace/packages
-pushd /workspace/packages
-git clone https://github.com/FinMind/FinMind.git
-git branch
-popd
